92559a2d71e2007429edcadfcc9010c7fd25d99a
yashma-sonara <yashma.sonara04@gmail.com>
2024-03-14 21:24:28 +0800

    fix nav

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,22 +1,27 @@
 import { NavLink } from "react-router-dom";
 import "./header.css";
 const Header = () => {
+    const page1 = <NavLink className="header_menu" style={({ isActive }) => {
+        return {
+            color: isActive ? "#8DCAFE" : "rgba(255, 255, 255, 0.6)",
+            textDecoration: isActive ? "underline" : "none",
+        };
+    } } to="/">Home</NavLink>;
+    
+    
+    const page2 = <NavLink className="header_menu" style={({ isActive }) => {
+        return {
+            color: isActive ? "#8DCAFE" : "rgba(255, 255, 255, 0.6)",
+            textDecoration: isActive ? "underline" : "none",
+        };
+    } } to="/page2">Settings</NavLink>;
+    
     return(
         <div className="header">
             <div className="header_wrapper">
                 <div className="header_title">Notification Task</div>
-                <NavLink className="header_menu" style={({ isActive }) => {
-                    return {
-                        color: isActive ? "#8DCAFE" : "rgba(255, 255, 255, 0.6)",
-                        textDecoration: isActive? "underline" : "none",
-                    };
-                }} to="/">Home</NavLink>
-                <NavLink className="header_menu" style={({ isActive }) => {
-                    return {
-                        color: isActive ? "#8DCAFE" : "rgba(255, 255, 255, 0.6)",
-                        textDecoration: isActive? "underline" : "none",
-                    };
-                }} to="/page2">Settings</NavLink>
+                {page1}
+                {page2}
             </div>
 
         </div>
